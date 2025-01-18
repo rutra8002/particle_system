064c4ae8d2fc433e625650d0d09f7cb397a52262
ruter <arturkummer08@gmail.com>
2025-01-18 09:45:11 +0100

    yes

--- setup.py
+++ setup.py
@@ -2,8 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name='particle_systemszcz',
-    version='1.0.0',
-    packages=find_packages(),
+    version='1.0.3',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
     install_requires=[
         'pygame-ce',
     ],
