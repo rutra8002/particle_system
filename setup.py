4424093b05cec2d7cb276a20677baed9a646a090
ruter <arturkummer08@gmail.com>
2025-01-18 09:27:55 +0100

    1.0 version

--- setup.py
+++ setup.py
@@ -1,21 +1,20 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='particle_system',
+    name='particle_systemszcz',
     version='1.0.0',
     packages=find_packages(),
     install_requires=[
         'pygame-ce',
     ],
     author='rutra',
-    author_email='no',
+    author_email='ruterszcz@gmail.com',
     description='A particle system library',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/yourusername/particle_system',
+    url='https://github.com/rutra8002/particle_system',
     classifiers=[
         'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
     python_requires='>=3.6',
