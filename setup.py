7f8d112f28f843e136c9cace26bb515dc205d863
ruter <arturkummer08@gmail.com>
2025-01-31 17:56:10 +0100

    Revert "v change"
    This reverts commit 3e9a37c1f8bc6c1c60eb10e8907cc37f88ecb5ad.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='particle_systemszcz',
-    version='1.3.2',
+    version='1.3.1',
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
     install_requires=[
