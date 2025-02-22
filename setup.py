c0e9cbab0f05469a7f2f18d27dd2544bbf33e850
ruter <arturkummer08@gmail.com>
2025-02-22 21:53:33 +0100

    version update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='particle_systemszcz',
-    version='1.3.4',
+    version='1.3.5',
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
     install_requires=[
