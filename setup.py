87449e7705b30ce7e5c69d0758262d7f2993102d
ruter <arturkummer08@gmail.com>
2025-02-28 10:00:02 +0100

    setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='particle_systemszcz',
-    version='1.3.5',
+    version='1.4.0',
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
     install_requires=[
